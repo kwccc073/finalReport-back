e5db696db6dd0af41fd97e36c3bf3b9c15b969a8
kaiwen <kaiwen073c@gmail.com>
2024-07-30 20:31:00 +0800

    0730

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
-import { create, login } from '../controllers/user.js'
+import { create, login, extend, profile, logout } from '../controllers/user.js'
 import * as auth from '../middlewares/auth.js'
 
 const router = Router()
 
 router.post('/', create)
+// 登入要經過auth.login的欄位檢查
 router.post('/login', auth.login, login)
+// 舊換新
+router.patch('/extend', auth.jwt, extend)
+// 取自己的資料
+router.get('/profile', auth.jwt, profile)
+// 登出
+router.delete('/logout', auth.jwt, logout)
 
 export default router
